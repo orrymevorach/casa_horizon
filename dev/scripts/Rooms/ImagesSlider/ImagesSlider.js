a4a35c7927777bb1cd34db0e3fd1253290c27eb4
orrymevorach <orry.mevorach@gmail.com>
2019-03-25 18:57:54 -0400

    image selector for jungle and beachfront container working

--- dev/scripts/Rooms/ImagesSlider/ImagesSlider.js
+++ dev/scripts/Rooms/ImagesSlider/ImagesSlider.js
@@ -7,38 +7,72 @@ class ImagesSlider extends React.Component {
         const titles = {
             beachfront: [
                 {
-                    "title": "Sunset"
+                    "title": "Sunset",
+                    "imageUrl": "../img/Rooms/cottage-sunset.jpg"
                 },
                 {
-                    "title": "Balcony"
+                    "title": "Balcony",
+                    "imageUrl": "../img/Rooms/cottage-balcony.jpg"
                 },
                 {
-                    "title": "Living Room"
+                    "title": "Living Room",
+                    "imageUrl": "../img/Rooms/cottage-inside.jpg"
                 },
                 {
-                    "title": "Master Bedroom"
+                    "title": "Master Bedroom",
+                    "imageUrl": "../img/Rooms/master.jpg"
                 },
                 {
-                    "title": "Kitchen"
+                    "title": "Kitchen",
+                    "imageUrl": "../img/Rooms/cottage-kitchen.jpg"
                 }
             ],
             jungle: [
                 {
-                    "title": "Jungle House"
+                    "title": "Jungle House",
+                    "imageUrl": "../img/Rooms/casa-courtyard.jpg"
                 },
                 {
-                    "title": "Living Room"
+                    "title": "Living Room",
+                    "imageUrl": "../img/Rooms/casa-living-room.jpg"
                 },
                 {
-                    "title": "Bedroom"
+                    "title": "Bedroom",
+                    "imageUrl": "../img/Rooms/Room1.jpg"
                 },
                 {
-                    "title": "Yoga Loft"
+                    "title": "Yoga Loft",
+                    "imageUrl": "../img/Rooms/yoga-loft.jpg"
                 },
                 {
-                    "title": "Shared Kitchen"
+                    "title": "Shared Kitchen",
+                    "imageUrl": "../img/Rooms/casa-kitchen.jpg"
                 }
             ]
+        }
+
+        const { section } = this.props
+
+        function selectImage(e) {
+            let imageUrl;
+            
+            if(e.target.parentElement.className === "overlay") {
+                imageUrl = e.target.parentElement.parentElement.dataset.imageurl
+            }
+            else if (e.target.parentElement.classList.contains("image-container")) {
+                imageUrl = e.target.parentElement.dataset.imageurl
+            }
+            console.log(imageUrl)
+            
+            if(section === "beachfront") {
+                const left = document.getElementsByClassName("beachfront-container")[0].children[0]
+                left.style["background-image"] = `url(${imageUrl})`
+            }
+            else if (section === "jungle") {
+                const left = document.getElementsByClassName("jungle-container")[0].children[0]
+                left.style["background-image"] = `url(${imageUrl})`
+            }
+
         }
 
         const arrayToMap = 
@@ -51,7 +85,12 @@ class ImagesSlider extends React.Component {
             <div className="images-slider">
                 {arrayToMap.map((section, index) => {
                     return (
-                        <div className={classNames("image-container", {"beachfront-image": this.props.section === "beachfront", "jungle-image": this.props.section === "jungle"})} key={index}>
+                        <div 
+                            className={classNames("image-container", {"beachfront-image": this.props.section === "beachfront", "jungle-image": this.props.section === "jungle"})} 
+                            key={index}
+                            data-imageurl={section.imageUrl}
+                            onClick={(e) => selectImage(e)}
+                        >
                             <div className="overlay">
                                 <h4>{section.title}</h4>
                             </div>
