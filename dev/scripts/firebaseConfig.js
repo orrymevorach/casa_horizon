820df122e44270023bba20aaade544389196b0ac
orrymevorach <orry.mevorach@gmail.com>
2019-03-30 21:26:03 -0400

    recentered header for all devices

--- dev/scripts/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyDKF69O_DQ5eatbEXy-H7lArnyoUqYTg4E",
-  authDomain: "casa-horizon.firebaseapp.com",
-  databaseURL: "https://casa-horizon.firebaseio.com",
-  projectId: "casa-horizon",
-  storageBucket: "casa-horizon.appspot.com",
-  messagingSenderId: "267399139758"
-};
