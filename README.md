242a10ad0f3dee3ffcbf99dfdf52eaf3ef47de62
orrymevorach <orry.mevorach@gmail.com>
2020-02-11 23:35:29 -0500

    update email info, add post action to contact form

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
+Casa Horizon
 # Getting Started
 
 1. `git clone` this repo
 2. `npm install`
 3. enjoy!
 
-Credit to HackerYou
-https://github.com/HackerYou/react-boilerplate
+To run in dev mode: run `gulp`
--- dev/scripts/Components/Footer/Footer.js
+++ dev/scripts/Components/Footer/Footer.js
@@ -12,9 +12,9 @@ const Footer = () => {
                 <h2>Stay Informed</h2>
                 <p>Get the latest updates on Casa Horizon</p>
                 <p className="margin-top">Sign up for our newsletter here</p>
-                <form action="#">
+                <form action="https://formspree.io/mnqdloag" method="POST">
                     <input type="email" className="margin-top" placeholder="Email Address" required />
-                    <input type="submit" value="Sign Up"/>
+                    <input type="submit" value="Sign Up" />
                 </form>
                 <div className="social-media-icons">
                     <a href="https://www.facebook.com/casahorizon/" target="blank">
@@ -30,24 +30,24 @@ const Footer = () => {
                 <form action="#">
                     <input type="text" placeholder="Name" />
                     <input type="email" placeholder="Email Address" required />
-                    <input type="textarea" placeholder="Message" required/>
-                    <input type="submit" value="Submit"/>
+                    <input type="textarea" placeholder="Message" required />
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
             <div className="column column4">
                 <h2>Get In Touch</h2>
                 <div className="email">
                     <p className="bold">Email us</p>
-                    <p>general@casahorizon.com</p>
+                    <p>info@casahorizon.com</p>
                 </div>
                 <div className="call">
                     <p className="bold">Call us toll free</p>
                     <p>(800) 403 - 5208</p>
                 </div>
             </div>
-            
+
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
