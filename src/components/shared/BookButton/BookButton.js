9d155e40b14d42f4b3bb32d343f8ca42ed94c65f
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-05-29 20:09:07 -0700

    book button mobile

--- src/components/shared/BookButton/BookButton.js
+++ src/components/shared/BookButton/BookButton.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useWindowSize from '../../../hooks/useWindowSize';
 
 export default function BookButton() {
+  const { isMobile } = useWindowSize({});
+  if (isMobile) return;
   return (
     <a href="/book-now" className="book-button">
       <button className="button">Book Now</button>
